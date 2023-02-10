209b3126e405307ed7d74ed9c03f30a7c95ef042
Logan-Staley <92904400+Logan-Staley@users.noreply.github.com>
2023-02-09 20:35:53 -0600

    commit

--- OperatingSystems/Server.c
+++ OperatingSystems/Server.c
@@ -1,21 +1,37 @@
-#include "unpipc.h"
-#include <fcntl.h>
-#define FIFO1 "/tmp/fifo.1"
-#define FIFO2 "/tmp/fifo.2"
-void server(int, int);
+#include <stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char **argv)
-{ int readfd, writefd;
-		pid_t childpid;
-/* create two FIFOs; OK if they already exist */
-		if ((mkfifo(FIFO1, FILE_MODE) < 0) && (errno != EEXIST))
-			err_sys("can't create %s", FIFO1);
-		if ((mkfifo(FIFO2, FILE_MODE) < 0) && (errno != EEXIST)) {
-			unlink(FIFO1);
-			err_sys("can't create %s", FIFO2);
-		}
-		readfd = Open(FIFO1, O_RDONLY, 0);
-		writefd = Open(FIFO2, O_WRONLY, 0);
-		server(readfd, writefd);
-		exit(0);
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
+
+
+int main(int argc, char* argv[]){
+	return 0;
 }
+
+
+
+
+
+// void server(int, int);
+
+// int main(int argc, char **argv)
+// { int readfd, writefd;
+// 		pid_t childpid;
+// /* create two FIFOs; OK if they already exist */
+// 		if ((mkfifo(FIFO1, FILE_MODE) < 0) && (errno != EEXIST))
+// 			err_sys("can't create %s", FIFO1);
+// 		if ((mkfifo(FIFO2, FILE_MODE) < 0) && (errno != EEXIST)) {
+// 			unlink(FIFO1);
+// 			err_sys("can't create %s", FIFO2);
+// 		}
+// 		readfd = Open(FIFO1, O_RDONLY, 0);
+// 		writefd = Open(FIFO2, O_WRONLY, 0);
+// 		server(readfd, writefd);
+// 		exit(0);
+// }
